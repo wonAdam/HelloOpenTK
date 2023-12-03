cc96ea9ad4bdf3aead9f5c2764bcd561013a8042
원동현 <ehdgus5500@gmail.com>
2023-12-03 16:53:32 +0900

    Creating a Window

---
+++ HelloOpenTK/MyGameWindow.cs
@@ -0,0 +1,28 @@
+﻿using OpenTK.Windowing.Common;
+using OpenTK.Windowing.Desktop;
+using OpenTK.Windowing.GraphicsLibraryFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOpenTK
+{
+    internal class MyGameWindow : GameWindow
+    {
+        public MyGameWindow(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
+        { 
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            base.OnUpdateFrame(e);
+
+            if (KeyboardState.IsKeyDown(Keys.Escape))
+            {
+                Close();
+            }
+        }
+    }
+}
--- HelloOpenTK/Program.cs
+++ HelloOpenTK/Program.cs
@@ -1,9 +1,17 @@
 ﻿using System;
+using HelloOpenTK;
+using OpenTK;
 
 public class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello ! OpenTK !");
+
+        // This line creates a new instance, and wraps the instance in a using statement so it's automatically disposed once we've exited the block.
+        using (MyGameWindow game = new MyGameWindow(800, 600, "LearnOpenTK"))
+        {
+            game.Run();
+        }
     }
 }
