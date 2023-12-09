301249665034ddda6dde44db76ebb6bcbae9fcae
원동현 <ehdgus5500@gmail.com>
2023-12-09 17:33:45 +0900

    Vertex & Triangle Class

--- HelloOpenTK/MyGameWindow.cs
+++ HelloOpenTK/MyGameWindow.cs
@@ -12,22 +12,22 @@ namespace HelloOpenTK
 {
     internal class MyGameWindow : GameWindow
     {
-        float[] vertices = {
-            -0.5f, -0.5f, 0.0f, //Bottom-left vertex
-             0.5f, -0.5f, 0.0f, //Bottom-right vertex
-             -0.5f,  0.5f, 0.0f  //Top-left vertex
+        Vertex[] vertices = {
+            new Vertex(-0.5f, -0.5f, 0.0f), //Bottom-left vertex
+            new Vertex(0.5f, -0.5f, 0.0f), //Bottom-right vertex
+            new Vertex(-0.5f,  0.5f, 0.0f),  //Top-left vertex
         };
 
-        float[] vertices2 = {
-            -0.5f, 0.5f, 0.0f, //top-left vertex
-             0.5f, 0.5f, 0.0f, //top-right vertex
-             0.5f,  -0.5f, 0.0f  //bottom-right vertex
+        Vertex[] vertices2 = {
+            new Vertex(-0.5f, 0.5f, 0.0f), //top-left vertex
+            new Vertex(0.5f, 0.5f, 0.0f), //top-right vertex
+            new Vertex(0.5f, -0.5f, 0.0f), //bottom-right vertex
         };
 
         Shader shader;
 
-        int VertexArrayObject;
-        int VertexArrayObject2;
+        Triangle t1;
+        Triangle t2;
 
         public MyGameWindow(int width, int height, string title) : base(GameWindowSettings.Default, new NativeWindowSettings() { Size = (width, height), Title = title }) 
         { 
@@ -46,17 +46,10 @@ namespace HelloOpenTK
         protected override void OnLoad()
         {
             base.OnLoad();
-            GL.ClearColor(0.0f, 0.0f, 1.0f, 1.0f);
+            GL.ClearColor(0.2f, 0.2f, 0.2f, 1.0f);
 
-            //////////////////////////////////////////////////////////
-            /// VBO (Vertex Buffer Object)
-            int VertexBufferObject;
-            // 그래픽카드에 Buffer (저장공간)를 생성
-            VertexBufferObject = GL.GenBuffer();
-            // 생성한 Buffer를 어떤 특성(ArrayBuffer)으로 선택
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
-            // 선택한 Buffer에 데이터를 보내기
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float), vertices, BufferUsageHint.StaticDraw);
+            t1 = new Triangle(vertices[0], vertices[1], vertices[2]);
+            t2 = new Triangle(vertices2[0], vertices2[1], vertices2[2]);
 
             //////////////////////////////////////////////////////////
             /// Shader
@@ -65,35 +58,6 @@ namespace HelloOpenTK
             string fragmentPath = Path.Join(ProjectDirectory, "fragment.glsl").ToString();
             shader = new Shader(vertexPath, fragmentPath);
 
-            /////////////////////////////////////////////////////////
-            /// VAO (Vertex Array Object)
-            // 그래픽카드에 VertexArrayObject를 저장할 공간 할당
-            VertexArrayObject = GL.GenVertexArray();
-            // VertexArrayObject를 선택
-            GL.BindVertexArray(VertexArrayObject);
-            // 그래픽카드에 Bind된 Buffer의 해석 방법을 Bind된 VertexArrayObject에 저장
-            // 0번 속성, 데이터는 3개, float 타입, ...
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-            // 0번 속성을 Enable
-            GL.EnableVertexAttribArray(0);
-
-            int VertexBufferObject2;
-            // 그래픽카드에 Buffer (저장공간)를 생성
-            VertexBufferObject2 = GL.GenBuffer();
-            // 생성한 Buffer를 어떤 특성(ArrayBuffer)으로 선택
-            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject2);
-            // 선택한 Buffer에 데이터를 보내기
-            GL.BufferData(BufferTarget.ArrayBuffer, vertices2.Length * sizeof(float), vertices2, BufferUsageHint.StaticDraw);
-
-            // 그래픽카드에 VertexArrayObject를 저장할 공간 할당
-            VertexArrayObject2 = GL.GenVertexArray();
-            // VertexArrayObject를 선택
-            GL.BindVertexArray(VertexArrayObject2);
-            // 그래픽카드에 Bind된 Buffer의 해석 방법을 Bind된 VertexArrayObject에 저장
-            // 0번 속성, 데이터는 3개, float 타입, ...
-            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
-            // 0번 속성을 Enable
-            GL.EnableVertexAttribArray(0);
         }
 
         protected override void OnRenderFrame(FrameEventArgs e)
@@ -102,11 +66,9 @@ namespace HelloOpenTK
             GL.Clear(ClearBufferMask.ColorBufferBit);
 
             shader.Use();
-            GL.BindVertexArray(VertexArrayObject);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
 
-            GL.BindVertexArray(VertexArrayObject2);
-            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+            t1.Draw();
+            t2.Draw();
 
             SwapBuffers();
         }
---
+++ HelloOpenTK/Triangle.cs
@@ -0,0 +1,69 @@
+﻿using OpenTK.Graphics.OpenGL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloOpenTK
+{
+    internal struct Vertex
+    {
+        public Vertex(float x, float y, float z)
+        {
+            _x = x;
+            _y = y;
+            _z = z;
+        }
+
+        public float _x;
+        public float _y;
+        public float _z;
+    }
+
+    internal class Triangle
+    {
+        int VertexBufferObject;
+
+        int VertexArrayObject;
+
+        Vertex[] vertices = new Vertex[3];
+
+        public Triangle(Vertex v1, Vertex v2, Vertex v3) 
+        {
+            vertices[0] = v1;
+            vertices[1] = v2;
+            vertices[2] = v3;
+
+            //////////////////////////////////////////////////////////
+            /// VBO (Vertex Buffer Object)
+            // 그래픽카드에 Buffer (저장공간)를 생성
+            VertexBufferObject = GL.GenBuffer();
+            // 생성한 Buffer를 어떤 특성(ArrayBuffer)으로 선택
+            GL.BindBuffer(BufferTarget.ArrayBuffer, VertexBufferObject);
+            // 선택한 Buffer에 데이터를 보내기
+            GL.BufferData(BufferTarget.ArrayBuffer, vertices.Length * sizeof(float) * 3, vertices, BufferUsageHint.StaticDraw);
+
+            /////////////////////////////////////////////////////////
+            /// VAO (Vertex Array Object)
+            // 그래픽카드에 VertexArrayObject를 저장할 공간 할당
+            VertexArrayObject = GL.GenVertexArray();
+            // VertexArrayObject를 선택
+            GL.BindVertexArray(VertexArrayObject);
+            // 그래픽카드에 Bind된 Buffer의 해석 방법을 Bind된 VertexArrayObject에 저장
+            // 0번 속성, 데이터는 3개, float 타입, ...
+            GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, 3 * sizeof(float), 0);
+            // 0번 속성을 Enable
+            GL.EnableVertexAttribArray(0);
+
+            GL.BindBuffer(BufferTarget.ArrayBuffer, 0);
+            GL.BindVertexArray(0);
+        }
+
+        public void Draw()
+        {
+            GL.BindVertexArray(VertexArrayObject);
+            GL.DrawArrays(PrimitiveType.Triangles, 0, 3);
+        }
+    }
+}
